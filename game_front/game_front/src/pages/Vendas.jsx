012cf56c125cf24a0e7b821d026616521576f612
chsantos234 <77702696+chsantos234@users.noreply.github.com>
2023-11-30 14:45:13 -0300

    Add files via upload

---
+++ game_front/game_front/src/pages/Vendas.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import axios from 'axios'
+import { Button, Input } from "@chakra-ui/react"
+
+const Vendas = () => {
+  const navigate = useNavigate()
+  const [theme, setTheme] = useState('')
+  const [game, setGame] = useState('')
+  const [gameId,setGameId] = useState(none)
+
+
+  const getTheme = async () => { }
+  
+  const getGame = async () => { }
+  
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Vendas
