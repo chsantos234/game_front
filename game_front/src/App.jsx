e0fb76db2bcd4ac7c2fb4037119c4560f5839bb0
chsantos234 <camilosantos1709@gmail.com>
2023-11-30 00:47:55 -0300

    feat: socorro muita coisa

--- game_front/src/App.jsx
+++ game_front/src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
+  async function get_json() {
+    await axios.get("http://localhost:5173/")
+      .then(function (response) {
+        setResp(response)
+      })
+      .catch(function (error) {
+        console.error(error);
+      })
+  }
+
   const [count, setCount] = useState(0)
   const [count1, setCount1] = useState(0)
   const [count2, setCount2] = useState(0)
+  const [resp,setResp] = useState()
 
   return (
     <>
