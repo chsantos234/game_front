281e830a44179be60ce091b96d01ace663307102
CamiloHMS <camilo.santos@icen.ufpa.br>
2023-11-30 11:06:57 -0300

    feat: home page

--- game_front/src/App.jsx
+++ game_front/src/App.jsx
@@ -1,8 +1,23 @@
-import { useState } from 'react'
-import axios from 'axios'
+//import { useState } from 'react'
+//import axios from 'axios'
 import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+
+import Home from './pages/Home'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  }
+])
 
 function App() {
+/*
   async function get_json() {
     await axios.get("http://localhost:5173/")
       .then(function (response) {
@@ -13,22 +28,11 @@ function App() {
       })
   }
 
-  const [count, setCount] = useState(0)
-  const [count1, setCount1] = useState(0)
-  const [count2, setCount2] = useState(0)
   const [resp,setResp] = useState()
-
+*/
   return (
     <>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-      <button onClick={() => setCount1((count1) => count1 + 1)}>
-        count is {count1}
-      </button>
-      <button onClick={() => setCount2((count2) => count2 + 1)}>
-        count is {count2}
-      </button>
+      <RouterProvider router={router}/>
     </>
   )
 }
