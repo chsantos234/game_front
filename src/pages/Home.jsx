b6d13c6c855e020513777ec854d455f9bf11fcda
CamiloHMS <camilo.santos@icen.ufpa.br>
2023-12-04 13:37:42 -0300

    feat: home layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,32 +7,36 @@ const Home = () => {
   return (
     <div className='h-screen overflow-y-hidden flex flex-col justify-between items-center gap-10'>
       <h1 className='text-6xl font-medium mb-7 mt-40 font-Croissant'>Escolha uma das opções</h1>
-      <ul className='flex gap-y-12 bg-slate-400 p-10 rounded-lg items-center text-lg text-gray-100 font-medium flex-col'>
-        <li className='flex flex-col gap-4 items-center'>
+      <ul1 className='flex gap-y-12 bg-slate-400 p-10 rounded-lg items-center text-lg text-gray-100 font-medium flex-col'>
+        <li1 className='flex flex-col gap-4 items-center'>
           <p>
             Ver preços entre vendas
           </p>
           <Button colorScheme='blue' className='w-fit' onClick={() => navigate('/vendas')}>
             Clique
           </Button>
-        </li>
-        <li className='flex flex-col gap-4 items-center'>
+        </li1>
+      </ul1>
+      <ul2 className='flex gap-y-12 bg-slate-400 p-10 rounded-lg items-center text-lg text-gray-100 font-medium flex-col'>
+        <li2 className='flex flex-col gap-4 items-center'>
           <p>
             Ver descrição / notas de um jogo
           </p>
           <Button colorScheme='blue' className='w-fit' onClick={() => navigate('/notas')}>
             Clique
           </Button>
-        </li>
-        <li className='flex flex-col gap-4 items-center'>
+        </li2>
+      </ul2>
+      <ul3 className='flex gap-y-12 bg-slate-400 p-10 rounded-lg items-center text-lg text-gray-100 font-medium flex-col'>
+        <li3 className='flex flex-col gap-4 items-center'>
           <p>
             Ver histórico de preços na steam
           </p>
           <Button colorScheme='blue' className='w-fit' onClick={() => navigate('/historico')}>
             Clique
           </Button>
-        </li>
-      </ul>
+        </li3>
+      </ul3>
     </div>
   )
 }
