ef771bb5aea1bad442f642fce0d8cc560195bf60
CamiloHMS <camilo.santos@icen.ufpa.br>
2023-12-06 12:54:00 -0300

    feat: hist code

--- src/pages/Desc.jsx
+++ src/pages/Desc.jsx
@@ -47,23 +47,23 @@ const Desc = () => {
           Voltar
         </Button>
       </div>
-      :
-      !details ? <div className="flex flex-col gap-8 items-center justify-center">
-        <div className='text-justify' dangerouslySetInnerHTML={{ __html: music.replaceAll('\n', '<br />') }} />
-        <div className='flex gap-1'>
-          <Input placeholder='Digite o ID de alguma música da lista acima' width={430} onChange={(e) => setLyricsId(e.target.value)} />
-          <Button colorScheme='green' onClick={getLyric}>
-            Buscar
-          </Button>
+        :
+        !details ? <div className="flex flex-col gap-8 items-center justify-center">
+          <div className='text-justify' dangerouslySetInnerHTML={{ __html: music.replaceAll('\n', '<br />') }} />
+          <div className='flex gap-1'>
+            <Input placeholder='Digite o ID de alguma música da lista acima' width={430} onChange={(e) => setLyricsId(e.target.value)} />
+            <Button colorScheme='green' onClick={getLyric}>
+              Buscar
+            </Button>
+          </div>
         </div>
-      </div>
-      :
-      <div className="flex flex-col gap-6">
-        <Button colorScheme='yellow' width={70} onClick={() => navigate('/')}>
-          Voltar
-        </Button>
-        <div dangerouslySetInnerHTML={{ __html: lyrics }} />
-      </div>}
+          :
+          <div className="flex flex-col gap-6">
+            <Button colorScheme='yellow' width={70} onClick={() => navigate('/')}>
+              Voltar
+            </Button>
+            <div dangerouslySetInnerHTML={{ __html: lyrics }} />
+          </div>}
     </div>
   )
 }
--- src/pages/Hist.jsx
+++ src/pages/Hist.jsx
@@ -4,12 +4,48 @@ import axios from 'axios'
 import { Button, Input } from "@chakra-ui/react"
 
 const Hist = () => {
+  const [theme, setTheme] = useState('') // pesquisa
+  const [gameId, setGameId] = useState(null) // escolha do usuário
 
-    return (
-        <div>
+  const [gameTodayStore, setGameTodayStore] = useState('')
+  const [gameTodayFormatted, setGameTodayFormatted] = useState('')
+  const [gameLowestStore, setGameLowestStore] = useState('')
+  const [gameLowestFormatted, setGameLowestFormatted] = useState('')
+  const [gamePriceUrl, setGamePriceUrl] = useState('')
+  const [gameLowestDate, setGameLowestDate] = useState('')
 
+  return (
+    <div className='min-h-screen flex flex-col justify-center items-center'>
+      {!music ? <div className="flex flex-col justify-center items-center gap-8">
+        <div className='flex gap-1'>
+          <Input placeholder='Digite o nome do(a) artista' size='md' width={230} onChange={(e) => setArtist(e.target.value)} />
+          <Button colorScheme='green' onClick={getMusic}>
+            Buscar
+          </Button>
         </div>
-    )
+        <Button colorScheme='blue' width={70} onClick={() => navigate('/')}>
+          Voltar
+        </Button>
+      </div>
+      :
+      !lyrics ? <div className="flex flex-col gap-8 items-center justify-center">
+        <div className='text-justify' dangerouslySetInnerHTML={{ __html: music.replaceAll('\n', '<br />') }} />
+        <div className='flex gap-1'>
+          <Input placeholder='Digite o ID de alguma música da lista acima' width={430} onChange={(e) => setLyricsId(e.target.value)} />
+          <Button colorScheme='green' onClick={getLyric}>
+            Buscar
+          </Button>
+        </div>
+      </div>
+      :
+      <div className="flex flex-col gap-6">
+        <Button colorScheme='yellow' width={70} onClick={() => navigate('/')}>
+          Voltar
+        </Button>
+        <div dangerouslySetInnerHTML={{__html: lyrics}} />
+      </div>}
+     </div> 
+  )
 }
 
 export default Hist
